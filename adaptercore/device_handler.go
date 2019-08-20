c026fdb2130970cbe246b742881e909f10418514
William Kurkian <wkurkian3@gmail.com>
2019-08-20 10:34:30 -0400

    VOL-1863 Changing the omci indication forwarding to happen in a go routine
    Change-Id: I55c67f6ae22e422f9ed9412e5e2f29a3d082f3fc

--- adaptercore/device_handler.go
+++ adaptercore/device_handler.go
@@ -237,6 +237,7 @@ func (dh *DeviceHandler) addPort(intfID uint32, portType voltha.Port_PortType, s
 
 // readIndications to read the indications from the OLT device
 func (dh *DeviceHandler) readIndications() {
+	defer log.Errorw("Indications ended", log.Fields{})
 	indications, err := dh.Client.EnableIndication(context.Background(), new(oop.Empty))
 	if err != nil {
 		log.Errorw("Failed to read indications", log.Fields{"err": err})
@@ -335,9 +336,7 @@ func (dh *DeviceHandler) handleIndication(indication *oop.Indication) {
 	case *oop.Indication_OmciInd:
 		omciInd := indication.GetOmciInd()
 		log.Infow("Received Omci indication ", log.Fields{"OmciInd": omciInd})
-		if err := dh.omciIndication(omciInd); err != nil {
-			log.Errorw("send-omci-indication-errr", log.Fields{"error": err, "omciInd": omciInd})
-		}
+		go dh.omciIndication(omciInd)
 	case *oop.Indication_PktInd:
 		pktInd := indication.GetPktInd()
 		log.Infow("Received pakcet indication ", log.Fields{"PktInd": pktInd})
@@ -601,7 +600,7 @@ func (dh *DeviceHandler) GetOfpPortInfo(device *voltha.Device, portNo int64) (*i
 	}, nil
 }
 
-func (dh *DeviceHandler) omciIndication(omciInd *oop.OmciIndication) error {
+func (dh *DeviceHandler) omciIndication(omciInd *oop.OmciIndication) {
 	log.Debugw("omci indication", log.Fields{"intfID": omciInd.IntfId, "onuID": omciInd.OnuId})
 	var deviceType string
 	var deviceID string
@@ -618,7 +617,7 @@ func (dh *DeviceHandler) omciIndication(omciInd *oop.OmciIndication) error {
 		onuDevice, err := dh.coreProxy.GetChildDevice(context.TODO(), dh.device.Id, kwargs)
 		if err != nil {
 			log.Errorw("onu not found", log.Fields{"intfID": omciInd.IntfId, "onuID": omciInd.OnuId})
-			return err
+			return
 		}
 		deviceType = onuDevice.Type
 		deviceID = onuDevice.Id
@@ -638,9 +637,9 @@ func (dh *DeviceHandler) omciIndication(omciInd *oop.OmciIndication) error {
 		ic.InterAdapterMessageType_OMCI_REQUEST, dh.deviceType, deviceType,
 		deviceID, proxyDeviceID, ""); sendErr != nil {
 		log.Errorw("send omci request error", log.Fields{"fromAdapter": dh.deviceType, "toAdapter": deviceType, "onuID": deviceID, "proxyDeviceID": proxyDeviceID})
-		return sendErr
+		return
 	}
-	return nil
+	return
 }
 
 //ProcessInterAdapterMessage sends the proxied messages to the target device
