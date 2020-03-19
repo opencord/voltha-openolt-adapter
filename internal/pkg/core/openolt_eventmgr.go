ac15959c46136a5565c97d336e4757056196f217
Devmalya Paul <Devmalya.Paul@radisys.com>
2020-03-19 03:01:39 -0400

    VOL-897 Onu Differential Reach Exceeded Alarm
            Added support to raise ONU differential reach exceeded
            alarm event
    
    Change-Id: I859b87d4c6822422165626e71c22f524f1a0d753

--- internal/pkg/core/openolt_eventmgr.go
+++ internal/pkg/core/openolt_eventmgr.go
@@ -56,6 +56,7 @@ const (
 	onuLossOfGEMChannelDelineationEvent = "ONU_LOSS_OF_GEM_CHANNEL_DELINEATION"
 	onuPhysicalEquipmentErrorEvent      = "ONU_PHYSICAL_EQUIPMENT_ERROR"
 	onuLossOfAcknowledgementEvent       = "ONU_LOSS_OF_ACKNOWLEDGEMENT"
+	onuDifferentialReachExceededEvent   = "ONU_DIFFERENTIAL_REACH_EXCEEDED"
 )
 
 const (
@@ -702,5 +703,30 @@ func (em *OpenOltEventMgr) onuLossOfAcknowledgementIndication(onuLOA *oop.OnuLos
 		return err
 	}
 	logger.Debugw("ONU physical equipment error event sent to KAFKA", log.Fields{"onu-id": onuLOA.OnuId, "intf-id": onuLOA.IntfId})
+	return nil
+}
+
+func (em *OpenOltEventMgr) onuDifferentialReachExceededIndication(onuDRE *oop.OnuDifferentialReachExceededIndication, deviceID string, raisedTs int64) error {
+	/* Populating event context */
+	context := map[string]string{
+		"onu-id":  strconv.FormatUint(uint64(onuDRE.OnuId), base10),
+		"intf-id": strconv.FormatUint(uint64(onuDRE.IntfId), base10),
+		"differential-distance": strconv.FormatUint(uint64(onuDRE.DifferentialDistance), base10),
+	}
+	/* Populating device event body */
+	de := &voltha.DeviceEvent{
+		Context:    context,
+		ResourceId: deviceID,
+	}
+	if onuDRE.Status == statusCheckOn {
+		de.DeviceEventName = fmt.Sprintf("%s_%s", onuDifferentialReachExceededEvent, "RAISE_EVENT")
+	} else {
+		de.DeviceEventName = fmt.Sprintf("%s_%s", onuDifferentialReachExceededEvent, "CLEAR_EVENT")
+	}
+	/* Send event to KAFKA */
+	if err := em.eventProxy.SendDeviceEvent(de, equipment, onu, raisedTs); err != nil {
+		return err
+	}
+	log.Debugw("ONU differential reach exceeded event sent to KAFKA", log.Fields{"onu-id": onuDRE.OnuId, "intf-id": onuDRE.IntfId})
 	return nil
 }
