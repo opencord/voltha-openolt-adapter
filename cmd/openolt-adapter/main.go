b7dd2a5451e75f375c541744a5d8f89ba40e3f06
Girish Kumar <girish_kumar@infosys.com>
2020-06-15 14:51:10 +0000

    [VOL-3142] Enabled tracing for openolt-adapter and added config parameters
    Change-Id: Ia451b39b84fe4f4906aa5f7fef679354cc9a4294

--- cmd/openolt-adapter/main.go
+++ cmd/openolt-adapter/main.go
@@ -509,6 +509,13 @@ func main() {
 
 	probeCtx := context.WithValue(ctx, probe.ProbeContextKey, p)
 
+	closer, err := log.InitTracingAndLogCorrelation(cf.TraceEnabled, cf.TraceAgentAddress, cf.LogCorrelationEnabled)
+	if err != nil {
+		logger.Warnw("unable-to-initialize-jaeger-tracing", log.Fields{"error": err})
+	} else {
+		defer closer.Close()
+	}
+
 	go ad.start(probeCtx)
 
 	code := waitForExit()
