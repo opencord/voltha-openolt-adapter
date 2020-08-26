50119d0f51f04993bb8c1b859da04eb916fe0e64
kesavand <kesavan.duraiswamy@gmail.com>
2020-08-26 15:07:47 +0530

    VOL-3395  Encryption of the OMCC GEM Change-Id: I8d4bfaab2d68b765f2c4818db8fbfe37fc910fc0

--- pkg/mocks/mockOpenOltClient.go
+++ pkg/mocks/mockOpenOltClient.go
@@ -22,6 +22,7 @@ import (
 	"errors"
 	"io"
 
+	config "github.com/opencord/voltha-protos/v3/go/ext/config"
 	openolt "github.com/opencord/voltha-protos/v3/go/openolt"
 	tech_profile "github.com/opencord/voltha-protos/v3/go/tech_profile"
 	"google.golang.org/grpc"
@@ -244,7 +245,7 @@ func (ooc *MockOpenoltClient) GetExtValue(ctx context.Context, in *openolt.Value
 }
 
 // OnuItuPonAlarmSet mocks the OnuItuPonAlarmSet function of Openoltclient.
-func (ooc *MockOpenoltClient) OnuItuPonAlarmSet(ctx context.Context, in *openolt.OnuItuPonAlarm, opts ...grpc.CallOption) (*openolt.Empty, error) {
+func (ooc *MockOpenoltClient) OnuItuPonAlarmSet(ctx context.Context, in *config.OnuItuPonAlarm, opts ...grpc.CallOption) (*openolt.Empty, error) {
 	return &openolt.Empty{}, nil
 }
 
