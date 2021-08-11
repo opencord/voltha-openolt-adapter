d69edd594619304ad9cba83258b7ca1cf1023df6
khenaidoo <knursimu@ciena.com>
2021-08-11 18:05:46 -0400

    [VOL-4292] WIP - Initial commit for gRPC migration
    Change-Id: I5af2125f2c2f53ffc78c474a94314bba408f8bae

--- go.mod
+++ go.mod
@@ -2,13 +2,23 @@ module github.com/opencord/voltha-openolt-adapter
 
 go 1.16
 
+replace github.com/opencord/voltha-protos/v4 => /Users/knursimu/work/go/src/github.com/opencord/voltha-protos
+
+replace github.com/opencord/voltha-lib-go/v6 => /Users/knursimu/work/go/src/github.com/opencord/voltha-lib-go
+
+replace github.com/coreos/bbolt v1.3.4 => go.etcd.io/bbolt v1.3.4
+
+replace go.etcd.io/bbolt v1.3.4 => github.com/coreos/bbolt v1.3.4
+
+replace google.golang.org/grpc => google.golang.org/grpc v1.25.1
+
 require (
 	github.com/cenkalti/backoff/v3 v3.1.1
-	github.com/gogo/protobuf v1.3.1
-	github.com/golang/protobuf v1.3.2
-	github.com/grpc-ecosystem/go-grpc-middleware v1.0.1-0.20190118093823-f849b5445de4
+	github.com/gogo/protobuf v1.3.2
+	github.com/golang/protobuf v1.5.2
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/opencord/voltha-lib-go/v6 v6.0.1
 	github.com/opencord/voltha-protos/v4 v4.2.0
-	go.etcd.io/etcd v0.0.0-20190930204107-236ac2a90522
-	google.golang.org/grpc v1.25.1
+	go.etcd.io/etcd v3.3.25+incompatible
+	google.golang.org/grpc v1.39.1
 )
---
+++ vendor/github.com/opencord/voltha-lib-go/v6/pkg/mocks/grpc/mock_grpc_server.go
@@ -0,0 +1,69 @@
+package grpc
+
+import (
+	"context"
+	vgrpc "github.com/opencord/voltha-lib-go/v4/pkg/grpc"
+	"github.com/opencord/voltha-lib-go/v4/pkg/log"
+	"github.com/opencord/voltha-lib-go/v4/pkg/probe"
+	"github.com/opencord/voltha-protos/v4/go/adapter_services"
+	"github.com/opencord/voltha-protos/v4/go/core"
+	"github.com/phayes/freeport"
+	"google.golang.org/grpc"
+	"strconv"
+)
+
+const (
+	mockGrpcServer = "mock-grpc-server"
+)
+
+type MockGRPCServer struct {
+	ApiEndpoint string
+	server      *vgrpc.GrpcServer
+	probe       *probe.Probe
+}
+
+func NewMockGRPCServer(ctx context.Context) (*MockGRPCServer, error) {
+	grpcPort, err := freeport.GetFreePort()
+	if err != nil {
+		return nil, err
+	}
+	s := &MockGRPCServer{
+		ApiEndpoint: "127.0.0.1:" + strconv.Itoa(grpcPort),
+		probe:       &probe.Probe{},
+	}
+	probePort, err := freeport.GetFreePort()
+	if err != nil {
+		return nil, err
+	}
+	probeEndpoint := "127.0.0.1:" + strconv.Itoa(probePort)
+	go s.probe.ListenAndServe(ctx, probeEndpoint)
+	s.probe.RegisterService(ctx, mockGrpcServer)
+	s.server = vgrpc.NewGrpcServer(s.ApiEndpoint, nil, false, s.probe)
+
+	logger.Infow(ctx, "mock-grpc-server-created", log.Fields{"endpoint": s.ApiEndpoint})
+	return s, nil
+}
+
+func (s *MockGRPCServer) AddCoreService(ctx context.Context, srv core.CoreServiceServer) {
+	s.server.AddService(func(server *grpc.Server) {
+		core.RegisterCoreServiceServer(server, srv)
+	})
+}
+
+func (s *MockGRPCServer) AddAdapterService(ctx context.Context, srv adapter_services.AdapterServiceServer) {
+	s.server.AddService(func(server *grpc.Server) {
+		adapter_services.RegisterAdapterServiceServer(server, srv)
+	})
+}
+
+func (s *MockGRPCServer) Start(ctx context.Context) {
+	s.probe.UpdateStatus(ctx, mockGrpcServer, probe.ServiceStatusRunning)
+	s.server.Start(ctx)
+	s.probe.UpdateStatus(ctx, mockGrpcServer, probe.ServiceStatusStopped)
+}
+
+func (s *MockGRPCServer) Stop() {
+	if s.server != nil {
+		s.server.Stop()
+	}
+}
