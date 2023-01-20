28bd2071af6f5c1aae74839c73c796c19b48cbd4
David Ferguson <daf@opennetworking.org>
2023-01-19 20:25:46 -0800

    another nop
    Change-Id: If92c7e8e45152bdd3cb2f39d0d730a10d9ce153e

--- makefiles/runit.sh
+++ makefiles/runit.sh
@@ -2,3 +2,28 @@ echo "# Nother NOP" >> nop
 git add .
 git commit -m "another nop"
 echo yes | git review
+echo "# Nother NOP" >> nop
+git add .
+git commit -m "another nop"
+echo yes | git review
+echo "# Nother NOP" >> nop
+git add .
+git commit -m "another nop"
+echo yes | git review
+echo "# Nother NOP" >> nop
+git add .
+git commit -m "another nop"
+echo yes | git review
+echo "# Nother NOP" >> nop
+git add .
+git commit -m "another nop"
+echo yes | git review
+echo "# Nother NOP" >> nop
+git add .
+git commit -m "another nop"
+echo yes | git review
+echo "# Nother NOP" >> nop
+git add .
+git commit -m "another nop"
+echo yes | git review
+
