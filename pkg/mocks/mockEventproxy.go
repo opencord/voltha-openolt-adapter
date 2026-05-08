6a4846a224d710f03f18f1669a88c8b06acc2eef
Abhay Kumar <abhay.kumar@radisys.com>
2026-05-08 10:29:15 +0000

    [VOL-5594] Reduce the number of kvstore clients
    Change-Id: I7747b0d5729dabf97764432592c5984e5b941bfe
    Signed-off-by: Abhay Kumar <abhay.kumar@radisys.com>

--- pkg/mocks/mockEventproxy.go
+++ pkg/mocks/mockEventproxy.go
@@ -47,6 +47,15 @@ func (me *MockEventProxy) SendKpiEvent(ctx context.Context, id string, deviceEve
 	return nil
 }
 
+// SendKpiEvent3 mocks the SendKpiEvent3 function
+func (me *MockEventProxy) SendKpiEvent3(ctx context.Context, id string, deviceEvent *voltha.KpiEvent3, category voltha.EventCategory_Types,
+	subCategory voltha.EventSubCategory_Types, raisedTs int64) error {
+	if raisedTs == 0 {
+		return errors.New("raisedTS cannot be zero")
+	}
+	return nil
+}
+
 // SendRPCEvent mocks the SendRPCEvent function
 func (me *MockEventProxy) SendRPCEvent(ctx context.Context, id string, deviceEvent *voltha.RPCEvent, category voltha.EventCategory_Types,
 	subCategory *voltha.EventSubCategory_Types, raisedTs int64) error {
@@ -78,14 +87,5 @@ func (me *MockEventProxy) Stop() {
 // SendDeviceEventWithKey mocks SendDeviceEventWithKey
 func (me *MockEventProxy) SendDeviceEventWithKey(ctx context.Context, deviceEvent *voltha.DeviceEvent, category eventif.EventCategory,
 	subCategory eventif.EventSubCategory, raisedTs int64, key string) error {
-	return nil
-}
-
-// SendKpiEvent3 mocks the SendKpiEvent3 function
-func (me *MockEventProxy) SendKpiEvent3(ctx context.Context, id string, deviceEvent *voltha.KpiEvent3, category eventif.EventCategory,
-	subCategory eventif.EventSubCategory, raisedTs int64) error {
-	if raisedTs == 0 {
-		return errors.New("raisedTS cannot be zero")
-	}
 	return nil
 }
